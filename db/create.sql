a89b17be6f6a34247e8061d85bd5f94b526c43da
kimberlytsai <115073626+kimberlytsai@users.noreply.github.com>
2024-02-02 09:47:22 +0000

    namen toevoegen + plaatje

--- db/create.sql
+++ db/create.sql
@@ -30,11 +30,12 @@ CREATE TABLE merken (
 --
 
 insert into products (name, description, merken, quantity, code, price) values ('Samyang Carbonara Buldak Hot Chicken Flavor Ramen ', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.','Buldak', '130','816905633-0', 2.65);
-insert into products (name, description, merken, quantity, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'Buldak', '130', '077030122-3', 11);
-insert into products (name, description, merken,quantity, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.','Buldak', '130', '445924201-X', 13.5);
+insert into products (name, description, merken, quantity, code, price) values ('Samyang Hot chicken ramen cheese', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'Buldak', '140', '077030122-3', 1.61);
+insert into products (name, description, merken, quantity, code, price) values ('Samyang Hot chicken ramen, 2x spicy', 'Pellentesque at nulla. Suspendisse potenti.','Buldak', '140', '445924201-X', 1.19);
 insert into products (name, description,merken, quantity, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.','Buldak','130',  '693155505-7', 13.5);
 insert into products (name, description,merken, quantity, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.','Buldak','130',  '686928463-6', 14);
 insert into products (name, description,merken, quantity, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.','Buldak','130', '492662523-7', 14);
+
 
 insert into merken (name, description) values ('Buldak', 'Hou jij van  pittig eten en van noedels. Koop dan Buldak noedels!');
 
