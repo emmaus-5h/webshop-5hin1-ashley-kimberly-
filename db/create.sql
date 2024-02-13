6afdc6fa2a16f71472208e6d72a9441caaaf6942
kimberlytsai <115073626+kimberlytsai@users.noreply.github.com>
2024-02-13 12:39:49 +0000

    tabellen toegevoegd (1:n)

--- db/create.sql
+++ db/create.sql
@@ -7,17 +7,44 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  merk_id INTEGER,
+  merken_id TEXT,
   price NUMERIC(10, 2),
-  quantity NUMERIC (5)
+  quantity_id NUMERIC (5)
+  pittigheid_id TEXT,
+  soortnoedel_id TEXT,
+  smaak_id TEXT
+  
   
 );
 
-CREATE TABLE merken (
+CREATE TABLE merken_id (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255),
   description TEXT
 ); 
+
+CREATE TABLE pittigheid_id (
+  id INTERGER PRIMARY KEY autoincrement,
+  hoeveelheid TEXT
+);
+
+CREATE TABLE smaak_id(
+  id INTERGER PRIMARY KEY autoincrement,
+  soort TEXT
+);
+
+CREATE TABLE soortnoedel_id(
+  id INTERGER PRIMARY KEY autoincrement,
+  dikte TEXT
+);
+
+CREATE TABLE quantity_id (
+  id INTERGER PRIMARY KEY autoincrement,
+  grootte TEXT 
+
+);
+
+
 
 
 --
@@ -29,18 +56,34 @@ CREATE TABLE merken (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, merk_id, quantity, code, price) values ('Samyang Carbonara Buldak Hot Chicken Flavor Ramen ', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.',1, '130','816905633-0', 2.65);
-insert into products (name, description, merk_id, quantity, code, price) values ('Samyang Hot chicken ramen cheese', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 1, '140', '077030122-3', 1.61);
-insert into products (name, description, merk_id, quantity, code, price) values ('Samyang Hot chicken ramen, 2x spicy', 'Pellentesque at nulla. Suspendisse potenti.',1, '140', '445924201-X', 1.19);
-insert into products (name, description, merk_id, quantity, code, price) values ('Indomie Instant noedels Kip Smaak', 'el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.',2,'70', '693155505-7', 13.5);
-insert into products (name, description, merk_id, quantity, code, price) values ('Indomie Instant noedels Garnalen Smaak', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',2,'70',  '686928463-6', 14);
-insert into products (name, description,merk_id, quantity, code, price) values ('Indomie Instant noedels Groenten Smaak', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.',2 ,'70', '492662523-7', 14);
+insert into products (name, description, merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Samyang Carbonara Buldak Hot Chicken Flavor Ramen ', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.',1, '130','816905633-0', 2.65);
+insert into products (name, description, merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Samyang Hot chicken ramen cheese', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 1, '140', '077030122-3', 1.61);
+insert into products (name, description, merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Samyang Hot chicken ramen, 2x spicy', 'Pellentesque at nulla. Suspendisse potenti.',1, '140', '445924201-X', 1.19);
+insert into products (name, description, merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Indomie Instant noedels Kip Smaak', 'el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.',2,'70', '693155505-7', 13.5);
+insert into products (name, description, merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Indomie Instant noedels Garnalen Smaak', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',2,'70',  '686928463-6', 14);
+insert into products (name, description,merken_id, quantity_id, code, price, pittigheid_id, soortnoedel_id, smaak_id) values ('Indomie Instant noedels Groenten Smaak', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.',2 ,'70', '492662523-7', 14);
 
+/* dit is de 1 op n relatie */
 
 insert into merken (name, description) values ('Buldak', 'Hou jij van  pittig eten en van noedels. Koop dan Buldak noedels!');
-  insert into merken (name, description) values ('Indomie', 'Lekkerste goedkope noedels! ');
+insert into merken (name, description) values ('Indomie', 'Lekkerste goedkope noedels! ');
 
-SELECT * FROM products, merken
+insert into pittigheid (hoeveelheid) values (ultiem pittig);
+
+insert into smaak (soort) values (kip);
+
+insert into soortnoedel_id (dikte) values (ramen);
+
+insert into quantity (grootte) values (70);
+
+
+
+
+
+
+
+
+/* SELECT * FROM products, merken
   WHERE products.merken.id = merken.id
 
-*/ nu moeten wij zorgen dat tabel products en tabel merken samen word gevoegd ookwel 1:m relatie/*
+nu moeten wij zorgen dat tabel products en tabel merken samen word gevoegd ookwel n:m relatie/*
